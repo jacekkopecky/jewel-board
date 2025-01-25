34d5d053e2e322954a95975a92d845455169896f
Jacek Kopecky <jacek@jacek.cz>
2025-01-25 23:24:22 +0000

    improve and add jewels
    they are now sized better as drawn

--- src/jewels.ts
+++ src/jewels.ts
@@ -9,10 +9,12 @@ export interface Jewel {
 }
 
 export const jewels: Jewel[] = [
-  { w: 2, h: 2, svg: 'svgs/2x2-green-triangle.svg' },
   { w: 1, h: 3, svg: 'svgs/1x3-blue.svg' },
   { w: 1, h: 2, svg: 'svgs/1x2-aqua.svg' },
   { w: 2, h: 2, svg: 'svgs/2x2-yellow.svg' },
+  { w: 2, h: 2, svg: 'svgs/2x2-orange.svg' },
+  { w: 2, h: 2, svg: 'svgs/2x2-green-triangle.svg' },
+  { w: 2, h: 3, svg: 'svgs/2x3-purple.svg' },
 ];
 
 export function selectJewels(areaSizes: number[]) {
